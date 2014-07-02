b4299609ef671f72c6ea33ba69a08aef221ab048
Ben Keith <ben@benlk.com>
2014-07-02 12:31:46 -0400

    Adds jszip

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ function clear_form(selector) {
 	$(selector).val('');
 };
 
+function create_zip(index_html) {
+	// loading a zip file
+	JSZipUtils.getBinaryContent("build.zip", function (err, data) {
+		if(err) {
+			throw err; // or handle the error
+		}
+		var zip = new JSZip(data);
+		zip.file("index.html", index_html);
+	});
+}
+
+
+
 $(document).ready(function(){
-	
 });
